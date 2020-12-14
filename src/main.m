00065094af0c9831669ecffec9271bae6f178ca1
Tobias Keller <Tobias.Keller@glasgow.ac.uk>
2020-12-14 09:11:40 +0100

    update and clean out main routine, add in initial ramp-up of applied background deformation and buoyancy forcing linearly over first 10 steps

--- src/main.m
+++ src/main.m
@@ -1,26 +1,24 @@
-% print start of run
+% print run header
 fprintf('\n\n*****  defail  |  %s  |  %s  *****\n\n',runID,datetime);
 
-% get smoothed random noise
+% produce smooth random perturbations
 rng(15);
 a = randn(N,N);
 for i = 1:max(smx,smz)
     a(2:end-1,2:end-1) = a(2:end-1,2:end-1) ...
                        + smz./max(smx,smz).*diff(a(:,2:end-1),2,1)./8 ...
                        + smx./max(smx,smz).*diff(a(2:end-1,:),2,2)./8;
-    a([1 end],:) = (a(round(N/8)+[0 1],:)+a(round(N*7/8)+[0,1],:))/2;
-    a(:,[1 end]) = (a(:,round(N/8)+[0 1])+a(:,round(N*7/8)+[0,1]))/2;
+    a([1 end],:) = a([end-1 2],:);
+    a(:,[1 end]) = a(:,[end-1 2]);
 end
-a([1 end],:) = a([end-1 2],:);
-a(:,[1 end]) = a(:,[end-1 2]);
 a = a./max(abs(a(:)));
 
 % get coordinate arrays
 x     = -L/2-h/2:h:L/2+h/2;
 z     = -L/2-h/2:h:L/2+h/2;
-[X,Z] = meshgrid(x,z);
 xc    = (x(1:end-1)+x(2:end))./2;
 zc    = (z(1:end-1)+z(2:end))./2;
+[X,Z] = meshgrid(x,z);
 
 % initialise solution and residual fields
 csw  =  0;
@@ -30,31 +28,35 @@ else
     f  =  1 + f2.*exp(-(X+xpos).^2./wx^2).*exp(-(Z+zpos).^2./wz^2);
 end
 f      =  f + a.*f1;  fo = f;  fi = f;  res_f = 0.*f;  fmass0 = f0.*sum(f(:));
-UP     = -(X(:,1:end-1)+X(:,2:end))/2/L*Pu*L;
-US     = -(Z(:,1:end-1)+Z(:,2:end))/2/L*Si*L;  UBG = UP+US;
 WP     =  (Z(1:end-1,:)+Z(2:end,:))/2/L*Pu*L;
-WS     = -(X(1:end-1,:)+X(2:end,:))/2/L*Si*L;  WBG = WP+WS+csw;
-clear WP WS UP US
-U      =  0.*UBG;  Ui = U;  res_U = 0.*U;
-W      =  0.*WBG;  Wi = W;  res_W = 0.*W;
+WS     = -(X(1:end-1,:)+X(2:end,:))/2/L*Si*L;  WBG0 = WP+WS+csw;
+UP     = -(X(:,1:end-1)+X(:,2:end))/2/L*Pu*L;
+US     = -(Z(:,1:end-1)+Z(:,2:end))/2/L*Si*L;  UBG0 = UP+US;     
+clear  WP WS UP US
+rmp    =  min(1,1/10);
+WBG = rmp.*WBG0;  UBG = rmp.*UBG0;  
+W      =  0.*WBG0;  Wi = W;  res_W = 0.*W;
+U      =  0.*UBG0;  Ui = U;  res_U = 0.*U;
 P      =  0.*f;  Pi = P;  res_P = 0.*P;  
 u      =  0.*U;
 w      =  0.*W;
-p      =  0.*P;  po = p;
+p      =  0.*P;
 
 % initialise parameter fields
-Div_V  =  0.*P;  Div_v = 0.*P;  Div_fV = 0.*P;  Div_fVBG = 0.*P;
-eIIref =  abs(Pu) + abs(Si) + 1e-6;  
-exx    =  0.*P - Pu;  ezz = 0.*P + Pu;  exz = zeros(N-1,N-1) - Si;  eII = 0.*P + abs(Pu) + abs(Si);  
-txx    =  0.*exx;  tzz = 0.*ezz;  txz = 0.*exz;  tII = 0.*eII;  tIIo = tII;
-etay   =  ones(size(P));
-zetay  =  ones(size(P))./(f0.*f).^m;
+ups    =  0.*P;  upss = 0.*P;  Div_fV = 0.*P;  Div_fVBG = 0.*P;
+eps0   =  abs(Pu) + abs(Si) + 1e-6;  
+exx    =  0.*P - Pu.*rmp;  ezz = 0.*P + Pu.*rmp;  exz = zeros(N-1,N-1) - Si.*rmp;  eps = 0.*P + (abs(Pu) + abs(Si)).*rmp;  
+txx    =  0.*exx;  tzz = 0.*ezz;  txz = 0.*exz;  tau = 0.*eps;
+eta    =  ones(size(P));
+zeta   =  ones(size(P))./(f0.*f).^m;
 yieldp =  zeros(size(P));
 yieldt =  Ty.*ones(size(P));
+
+% initialise timing parameters
 step   =  0;
 time   =  0;
+dt     =  h/500;
 dto    =  dt;
-it     =  0;  
 
 % overwrite fields from file if restarting run
 if     restart < 0  % restart from last continuation frame
@@ -71,12 +73,13 @@ end
 
 load ocean;  % load custom colormap
 
+% time stepping loop
 while time < tend && step < M
     
-    % plot results
+    % update parameters and plot results
     if ~mod(step,nop); up2date; output; end
     
-    % increment time/step
+    % increment time step
     time = time+dt;
     step = step+1;
     
@@ -86,27 +89,29 @@ while time < tend && step < M
     % store previous solution
     fo      = f;
     Div_fVo = Div_fV;  Div_fVBGo = Div_fVBG;
-    txxo    = txx; tzzo = tzz; txzo = txz;  
-    tIIo    = tII;
-    po      = p;
-    dto     = dt;
     
-    % reset residuals and iteration count
+    % update factor for ramp-up at beginning of run
+    rmp = min(1,step/10);
+    WBG = rmp.*WBG0;  UBG = rmp.*UBG0;  
+        
+    % reset residual norms and iteration count
     resnorm  = 1e3;
     resnorm0 = resnorm;
     it       = 0;
     
+    % initialise iterative solution guess
     Wi = W;  Ui = U;  Pi = P;  fi = f;
     
+    
     % non-linear iteration loop
-    while resnorm/resnorm0 >= rtol && resnorm >= atol && it <= maxit || it <= nup
+    while resnorm/resnorm0 >= rtol && resnorm >= atol && it <= maxit || it <= minit
                 
-        % store previous outer solution and residual fields   
+        % store previous iterative solution guess  
         Wii = Wi;  Wi = W;
         Uii = Ui;  Ui = U;
         Pii = Pi;  Pi = P;
         fii = fi;  fi = f;
-        
+                
         % update fields
         if ~mod(it,nup); up2date; end
 
@@ -120,13 +125,13 @@ while time < tend && step < M
             res_f([1 end],:) = res_f([end-1 2],:);                         % periodic boundaries
             res_f(:,[1 end]) = res_f(:,[end-1 2]);
             
-            if demean; res_f = res_f - mean(res_f(:)); end
+            if demean; res_f = res_f - mean(res_f(:)); end                 % remove mean from update
 
             f = fi - alpha.*res_f.*dt/10 + beta.*(fi-fii);                 % update solution
         end
         
         % update segregation velocities and compaction pressure
-        w   = - (K(1:end-1,:)+K(2:end,:)).*0.5 .* (diff(P,1,1)./h + B);    % z-segregation velocity
+        w   = -(K(1:end-1,:)+K(2:end,:)).*0.5 .* (diff(P,1,1)./h + B.*rmp);% z-segregation velocity
         w([1 end],:) = [sum(w([1 end],:),1)./2; ...                        % periodic boundaries
                         sum(w([1 end],:),1)./2];
         w(:,[1 end]) = w(:,[end-1 2]);
@@ -136,14 +141,13 @@ while time < tend && step < M
         u(:,[1 end]) = [sum(u(:,[1 end]),2)./2, ...
                         sum(u(:,[1 end]),2)./2];
                         
-        p   = max(ypcut,-zeta .* Div_V);                                  % compaction pressure
-%         p   = -zeta .* Div_V;                                  % compaction pressure
+        p   = -zeta .* ups;                                                % compaction pressure
 
         p([1 end],:) = p([end-1 2],:);                                     % periodic boundaries
         p(:,[1 end]) = p(:,[end-1 2]);
         
         % update z-reference velocity
-        Div_tz = diff(tzz(:,2:end-1),1,1)./h + diff(txz,1,2)./h;           % get z-stress divergence
+        Div_tz = diff(tzz(:,2:end-1),1,1)./h + diff(txz,1,2)./h;           % z-stress divergence
         
         res_W(:,2:end-1) = - Div_tz + diff(P(:,2:end-1),1,1)./h ...        % residual z-momentum equation
                                     + diff(p(:,2:end-1),1,1)./h;  
@@ -152,12 +156,12 @@ while time < tend && step < M
                             sum(res_W([1 end],:),1)./2];
         res_W(:,[1 end]) = res_W(:,[end-1 2]);
         
-        if demean; res_W = res_W - mean(res_W(:)); end
+        if demean; res_W = res_W - mean(res_W(:).*dtW(:))./dtW; end        % remove mean from update
         
         W = Wi - alpha.*res_W.*dtW + beta.*(Wi-Wii);                       % update solution
 
         % update x-reference velocity        
-        Div_tx  = diff(txx(2:end-1,:),1,2)./h + diff(txz,1,1)./h;          % get x-stress divergence
+        Div_tx  = diff(txx(2:end-1,:),1,2)./h + diff(txz,1,1)./h;          % x-stress divergence
         
         res_U(2:end-1,:) = - Div_tx + diff(P(2:end-1,:),1,2)./h ...
                                     + diff(p(2:end-1,:),1,2)./h;           % residual x-momentum equation
@@ -166,29 +170,29 @@ while time < tend && step < M
         res_U(:,[1 end]) = [sum(res_U(:,[1 end]),2)./2, ...
                             sum(res_U(:,[1 end]),2)./2];
 
-        if demean; res_U = res_U - mean(res_U(:)); end
+        if demean; res_U = res_U - mean(res_U(:).*dtU(:))./dtU; end        % remove mean from update
 
         U = Ui - alpha.*res_U.*dtU + beta.*(Ui-Uii);                       % update solution
         
         % update velocity divergences
-        Div_V(2:end-1,2:end-1) = diff(U(2:end-1,:),1,2)./h ...             % get velocity divergence
+        ups(2:end-1,2:end-1) = diff(U(2:end-1,:),1,2)./h ...               % velocity divergence
                                + diff(W(:,2:end-1),1,1)./h;
-        Div_V([1 end],:) = Div_V([end-1 2],:);                             % periodic boundaries
-        Div_V(:,[1 end]) = Div_V(:,[end-1 2]);
+        ups([1 end],:) = ups([end-1 2],:);                                 % periodic boundaries
+        ups(:,[1 end]) = ups(:,[end-1 2]);
         
-        Div_v(2:end-1,2:end-1) = diff(u(2:end-1,:),1,2)./h ...
+        upss(2:end-1,2:end-1) = diff(u(2:end-1,:),1,2)./h ...
                                + diff(w(:,2:end-1),1,1)./h;                % segregation velocity divergence
-        Div_v([1 end],:) = Div_v([end-1 2],:);                             % periodic boundaries         
-        Div_v(:,[1 end]) = Div_v(:,[end-1 2]);          
+        upss([1 end],:) = upss([end-1 2],:);                               % periodic boundaries         
+        upss(:,[1 end]) = upss(:,[end-1 2]);          
         
         % update strain rates
-        exx(:,2:end-1)   = diff(U,1,2)./h - Div_V(:,2:end-1)./3 - Pu;      % get x-normal strain rate
+        exx(:,2:end-1)   = diff(U,1,2)./h - ups(:,2:end-1)./3 - Pu.*rmp;   % x-normal strain rate
         exx([1 end],:)   = exx([end-1 2],:);                               % periodic boundaries
         exx(:,[1 end])   = exx(:,[end-1 2]);               
-        ezz(2:end-1,:)   = diff(W,1,1)./h - Div_V(2:end-1,:)./3 + Pu;      % get z-normal strain rate
+        ezz(2:end-1,:)   = diff(W,1,1)./h - ups(2:end-1,:)./3 + Pu.*rmp;   % z-normal strain rate
         ezz([1 end],:)   = ezz([end-1 2],:);                               % periodic boundaries
         ezz(:,[1 end])   = ezz(:,[end-1 2]);          
-        exz              = 1/2.*(diff(U,1,1)./h + diff(W,1,2)./h) - Si;    % get shear strain rate
+        exz              = 1/2.*(diff(U,1,1)./h+diff(W,1,2)./h) - Si.*rmp; % shear strain rate
         
         % update stresses
         txx = eta .* exx;                                                  % x-normal stress
@@ -196,45 +200,49 @@ while time < tend && step < M
         txz = etac.* exz;                                                  % xz-shear stress  
         
         % update reference pressure
-        res_P = Div_V + Div_v;                                             % residual mass equation
+        res_P = ups + upss;                                                % residual mass equation
 
         res_P([1 end],:) = res_P([end-1 2],:);                             % periodic boundaries
         res_P(:,[1 end]) = res_P(:,[end-1 2]);
         
-        if demean; res_P = res_P - mean(res_P(:)); end
+        if demean; res_P = res_P - mean(res_P(:).*dtP(:))./dtP; end        % remove mean from update
 
         P = Pi - alpha.*res_P.*dtP + beta.*(Pi-Pii);                       % update solution
         
-        % check and report convergence every nup iterations
-        if ~mod(it,nup); report; end
+        % check and report convergence every max(100,nup) iterations
+        if ~mod(it,max(100,nup)); report; end
         
         it = it+1;
 
     end
     
-    clear Wi Wii Ui Uii Pi Pii wi wii ui uii pi pii fi fii fo Div_Vo txxo tzzo txzo Div_tz Div_tx 
+    % clean workspace
+    clear Wi Wii Ui Uii Pi Pii wi wii ui uii pi pii fi fii fo Div_fVo Div_fVBGo Div_tz Div_tx dtW dtU dtP etac
     
     % print diagnostics
     fprintf(1,'\n         time to solution = %4.4f sec\n\n',toc);
 
-    fprintf(1,'         min U   = %s%4.4f;   mean U   = %s%4.4f;   max U   = %s%4.4f;\n'  ,int8(min(  U(:))<0),min(  U(:)),int8(mean(  U(:))<0),mean(  U(:)),int8(max(  U(:))<0),max(  U(:)));
-    fprintf(1,'         min W   = %s%4.4f;   mean W   = %s%4.4f;   max W   = %s%4.4f;\n'  ,int8(min( -W(:))<0),min( -W(:)),int8(mean( -W(:))<0),mean( -W(:)),int8(max( -W(:))<0),max( -W(:)));
-    fprintf(1,'         min P   = %s%4.4f;   mean P   = %s%4.4f;   max P   = %s%4.4f;\n\n',int8(min(  P(:))<0),min(  P(:)),int8(mean(  P(:))<0),mean(  P(:)),int8(max(  P(:))<0),max(  P(:)));
+    fprintf(1,'         min U    = %s%4.4f;   mean U    = %s%4.4f;   max U    = %s%4.4f;\n'  ,int8(min(  U(:))<0),min(  U(:)),int8(mean(  U(:))<0),mean(  U(:)),int8(max(  U(:))<0),max(  U(:)));
+    fprintf(1,'         min W    = %s%4.4f;   mean W    = %s%4.4f;   max W    = %s%4.4f;\n'  ,int8(min( -W(:))<0),min( -W(:)),int8(mean( -W(:))<0),mean( -W(:)),int8(max( -W(:))<0),max( -W(:)));
+    fprintf(1,'         min P    = %s%4.4f;   mean P    = %s%4.4f;   max P    = %s%4.4f;\n\n',int8(min(  P(:))<0),min(  P(:)),int8(mean(  P(:))<0),mean(  P(:)),int8(max(  P(:))<0),max(  P(:)));
 
-    fprintf(1,'         min u   = %s%4.4f;   mean u   = %s%4.4f;   max u   = %s%4.4f;\n'  ,int8(min(  u(:))<0),min(  u(:)),int8(mean(  u(:))<0),mean(  u(:)),int8(max(  f(:))<0),max(  u(:)));
-    fprintf(1,'         min w   = %s%4.4f;   mean w   = %s%4.4f;   max w   = %s%4.4f;\n'  ,int8(min( -w(:))<0),min( -w(:)),int8(mean( -w(:))<0),mean( -w(:)),int8(max(  f(:))<0),max( -w(:)));
-    fprintf(1,'         min p   = %s%4.4f;   mean p   = %s%4.4f;   max p   = %s%4.4f;\n\n',int8(min(  p(:))<0),min(  p(:)),int8(mean(  p(:))<0),mean(  p(:)),int8(max(  f(:))<0),max(  p(:)));
+    fprintf(1,'         min u    = %s%4.4f;   mean u    = %s%4.4f;   max u    = %s%4.4f;\n'  ,int8(min(  u(:))<0),min(  u(:)),int8(mean(  u(:))<0),mean(  u(:)),int8(max(  f(:))<0),max(  u(:)));
+    fprintf(1,'         min w    = %s%4.4f;   mean w    = %s%4.4f;   max w    = %s%4.4f;\n'  ,int8(min( -w(:))<0),min( -w(:)),int8(mean( -w(:))<0),mean( -w(:)),int8(max(  f(:))<0),max( -w(:)));
+    fprintf(1,'         min p    = %s%4.4f;   mean p    = %s%4.4f;   max p    = %s%4.4f;\n\n',int8(min(  p(:))<0),min(  p(:)),int8(mean(  p(:))<0),mean(  p(:)),int8(max(  f(:))<0),max(  p(:)));
     
-    fprintf(1,'         min f   = %s%4.4f;   mean f   = %s%4.4f;   max f   = %s%4.4f;\n'  ,int8(min(  f(:))<0),min(  f(:)),int8(mean(  f(:))<0),mean(  f(:)),int8(max(  f(:))<0),max(  f(:)));
-    fprintf(1,'         min K   = %s%4.4f;   mean K   = %s%4.4f;   max K   = %s%4.4f;\n\n',int8(min(  K(:))<0),min(  K(:)),int8(mean(  K(:))<0),mean(  K(:)),int8(max(  K(:))<0),max(  K(:)));
+    fprintf(1,'         min f    = %s%4.4f;   mean f    = %s%4.4f;   max f    = %s%4.4f;\n'  ,int8(min(  f(:))<0),min(  f(:)),int8(mean(  f(:))<0),mean(  f(:)),int8(max(  f(:))<0),max(  f(:)));
+    fprintf(1,'         min K    = %s%4.4f;   mean K    = %s%4.4f;   max K    = %s%4.4f;\n\n',int8(min(  K(:))<0),min(  K(:)),int8(mean(  K(:))<0),mean(  K(:)),int8(max(  K(:))<0),max(  K(:)));
     
-    fprintf(1,'         min eta = %s%4.4f;   mean eta = %s%4.4f;   max eta = %s%4.4f;\n'  ,int8(min(eta(:))<0),min(eta(:)),int8(mean(eta(:))<0),mean(eta(:)),int8(max(eta(:))<0),max(eta(:)));
-    fprintf(1,'         min eII = %s%4.4f;   mean eII = %s%4.4f;   max eII = %s%4.4f;\n'  ,int8(min(eII(:))<0),min(eII(:)),int8(mean(eII(:))<0),mean(eII(:)),int8(max(eII(:))<0),max(eII(:)));
-    fprintf(1,'         min tII = %s%4.4f;   mean tII = %s%4.4f;   max tII = %s%4.4f;\n\n',int8(min(tII(:))<0),min(tII(:)),int8(mean(tII(:))<0),mean(tII(:)),int8(max(tII(:))<0),max(tII(:)));
+    fprintf(1,'         min  eta = %s%4.4f;   mean  eta = %s%4.4f;   max  eta = %s%4.4f;\n'  ,int8(min( eta(:))<0),min( eta(:)),int8(mean( eta(:))<0),mean( eta(:)),int8(max( eta(:))<0),max( eta(:)));
+    fprintf(1,'         min zeta = %s%4.4f;   mean zeta = %s%4.4f;   max zeta = %s%4.4f;\n\n',int8(min(zeta(:))<0),min(zeta(:)),int8(mean(zeta(:))<0),mean(zeta(:)),int8(max(zeta(:))<0),max(zeta(:)));
+    
+    fprintf(1,'         min ups  = %s%4.4f;   mean ups  = %s%4.4f;   max ups  = %s%4.4f;\n'  ,int8(min(ups(:))<0),min(ups(:)),int8(mean(ups(:))<0),mean(ups(:)),int8(max(ups(:))<0),max(ups(:)));
+    fprintf(1,'         min eps  = %s%4.4f;   mean eps  = %s%4.4f;   max eps  = %s%4.4f;\n'  ,int8(min(eps(:))<0),min(eps(:)),int8(mean(eps(:))<0),mean(eps(:)),int8(max(eps(:))<0),max(eps(:)));
+    fprintf(1,'         min tau  = %s%4.4f;   mean tau  = %s%4.4f;   max tau  = %s%4.4f;\n\n',int8(min(tau(:))<0),min(tau(:)),int8(mean(tau(:))<0),mean(tau(:)),int8(max(tau(:))<0),max(tau(:)));
 
 end
 
 % plot results
-output; up2date; 
+up2date; output; 
 
 diary off
