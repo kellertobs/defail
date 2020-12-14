277d53a9bc6221066928770b789520eeeb9d786a
Tobias Keller <Tobias.Keller@glasgow.ac.uk>
2020-12-14 09:09:40 +0100

    update and clean out parameter update routine with rheology

--- src/up2date.m
+++ src/up2date.m
@@ -1,74 +1,62 @@
 % update tensor magnitudes
-eII(2:end-1,2:end-1) = (  (exx(2:end-1,2:end-1).^2 + ezz(2:end-1,2:end-1).^2 ...
+eps(2:end-1,2:end-1) = (  (exx(2:end-1,2:end-1).^2 + ezz(2:end-1,2:end-1).^2 ...
                      + 2.*(exz(1:end-1,1:end-1).^2 + exz(2:end  ,1:end-1).^2 ...
-                         + exz(1:end-1,2:end  ).^2 + exz(2:end  ,2:end  ).^2).*0.25)./2).^0.5 + 1e-16;
-eII([1 end],:) = eII([end-1 2],:);                                         % periodic boundaries
-eII(:,[1 end]) = eII(:,[end-1 2]);
+                         + exz(1:end-1,2:end  ).^2 + exz(2:end  ,2:end  ).^2).*0.25)./2).^0.5;
+eps([1 end],:) = eps([end-1 2],:);                                         % periodic boundaries
+eps(:,[1 end]) = eps(:,[end-1 2]);
 
-tII(2:end-1,2:end-1) = (  (txx(2:end-1,2:end-1).^2 + tzz(2:end-1,2:end-1).^2 ...
+tau(2:end-1,2:end-1) = (  (txx(2:end-1,2:end-1).^2 + tzz(2:end-1,2:end-1).^2 ...
                      + 2.*(txz(1:end-1,1:end-1).^2 + txz(2:end  ,1:end-1).^2 ...
-                         + txz(1:end-1,2:end  ).^2 + txz(2:end  ,2:end  ).^2).*0.25)./2).^0.5 + 1e-16;
-tII([1 end],:) = tII([end-1 2],:);                                         % periodic boundaries
-tII(:,[1 end]) = tII(:,[end-1 2]);
+                         + txz(1:end-1,2:end  ).^2 + txz(2:end  ,2:end  ).^2).*0.25)./2).^0.5;
+tau([1 end],:) = tau([end-1 2],:);                                         % periodic boundaries
+tau(:,[1 end]) = tau(:,[end-1 2]);
 
-% yieldt = max(    1e-6,  p + Ty );                                          % get Griffith yield stress
-% yieldp = max(-Ty+1e-6,-Ty + tII);                                          % get Griffith yield pressure
-% ypcut  = max(-Ty+1e-6,-Ty + tII.*(1-delta));
-
-yieldt = max(    1e-6,(p + tII + Ty)/2);                                     % get Griffith yield pressure
-yieldp = max(-Ty+1e-6,(p + tII - Ty)/2);                                     % get Griffith yield pressure
-ypcut  = max(-Ty+1e-6,(p + tII.*(1-delta) - Ty)/2);
+yieldt = max(    1e-3,(p + tau + Ty)/2);                                   % yield pressure
+yieldp = max(-Ty+1e-3,(p + tau - Ty)/2);                                   % yield pressure
 
 % update rheological parameters
-etav    = exp(-lmd.*f0.*(f-1)) ...
-        .* (eII./eIIref).^((1-nn)./nn);                                      % get shear viscosity
+etav   =  exp(-lmd.*f0.*(f-1)) ...
+      .* (eps./eps0./rmp).^((1-nn)./nn);                                   % shear viscosity
 
-t0 = etav.*eII;
+etay  =  yieldt./max(1e-16,eps);                                           % shear visco-plasticity
 
-etayi  =  etav.*(yieldt./max(1e-6,t0));
-for k  = 1:ceil(kappa)                                                     % regularise visco-plasticity
-    kk = delta/ceil(kappa);
-    etayi(2:end-1,2:end-1) = etayi(2:end-1,2:end-1) + kk.*(diff(etayi(2:end-1,:),2,2)+diff(etayi(:,2:end-1),2,1))./8;
-    etayi([1 end],:) = etayi([end-1 2],:);
-    etayi(:,[1 end]) = etayi(:,[end-1 2]);
+for k  = 1:ceil(kappa)                                                     % regularisation
+    kk = kappa/ceil(kappa);
+    etay(2:end-1,2:end-1) = etay(2:end-1,2:end-1) + kk.*(diff(etay(2:end-1,:),2,2)+diff(etay(:,2:end-1),2,1))./8;
+    etay([1 end],:) = etay([end-1 2],:);
+    etay(:,[1 end]) = etay(:,[end-1 2]);
 end
-etay  =  etayi.*(1-gamma) +  etay.*gamma;                                  % iterative relaxation
-eta   = (etav.^(-1/eps) + etay.^(-1/eps)).^(-eps) + etamin;
-% eta   = min(etav,etay) + etamin;
 
-zetav   = eta.*max(1e-6,(f0.*f)).^-m;                                      % get cmpct viscosity
-zetamin = etamin.*max(1e-6,(f0.*f)).^-m;                                      % get cmpct viscosity
-
-p0 = zetav.*Div_V;
-
-zetayi  = zetav.*((yieldp + Ty)./max(1e-6,p0 + Ty));
-for k  = 1:ceil(kappa)                                                     % regularise visco-plasticity
-    kk = delta/ceil(kappa);
-    zetayi(2:end-1,2:end-1) = zetayi(2:end-1,2:end-1) + kk.*(diff(zetayi(2:end-1,:),2,2)+diff(zetayi(:,2:end-1),2,1))./8;
-    zetayi([1 end],:) = zetayi([end-1 2],:);
-    zetayi(:,[1 end]) = zetayi(:,[end-1 2]);
-end
-zetay =  zetayi.*(1-gamma) +  zetay.*gamma;                                  % iterative relaxation
-
-zeta  = (zetav.^(-1/eps) + zetay.^(-1/eps)).^(-eps) + zetamin;
-% zeta = min(zetav,zetay) + zetamin;
-% zeta = eta.*max(1e-6,(f0.*f)).^-m;
-
-K    = f.^n;                                                               % get segregation coefficient
+eta  =  (1-gamma).*(min(etav,etay) + etamin) + gamma.*eta;                 % effective shear viscosity
 
 etac = (eta(1:end-1,1:end-1)+eta(2:end,1:end-1) ...                        % evaluate in cell corners
      +  eta(1:end-1,2:end  )+eta(2:end,2:end  )).*0.25;
+
+zetav   = etav  .*max(1e-6,f0.*f).^-m;                                     % cmpct viscosity
+zetamin = etamin.*max(1e-6,f0.*1).^-m;                                     % min cmpct viscosity
+
+zetay   = (yieldp+Ty)./max(1e-16,ups+Ty./zetav);                           % cmpct visco-plasticity
+
+for k  = 1:ceil(kappa)                                                     % regularisation
+    kk = kappa/ceil(kappa);
+    zetay(2:end-1,2:end-1) = zetay(2:end-1,2:end-1) + kk.*(diff(zetay(2:end-1,:),2,2)+diff(zetay(:,2:end-1),2,1))./8;
+    zetay([1 end],:) = zetay([end-1 2],:);
+    zetay(:,[1 end]) = zetay(:,[end-1 2]);
+end
+
+zeta = (1-gamma).*(min(zetav,zetay) + zetamin); + gamma.*zeta;             % effective cmpct viscosity
+
+K    = f.^n;                                                               % segregation coefficient
    
 % update iterative and physical time step sizes
 dtW = ((eta(1:end-1,:)+ eta(2:end,:)).*0.5./(h/2)^2 ...
-    + (zeta(1:end-1,:)+zeta(2:end,:)).*0.5./(h/2)^2).^-1;                  % iterative step size
+    + (zeta(1:end-1,:)+zeta(2:end,:)).*0.5./(h/2)^2).^-1;                  % W iterative step size
 dtU = ((eta(:,1:end-1)+ eta(:,2:end)).*0.5./(h/2)^2 ...
-    + (zeta(:,1:end-1)+zeta(:,2:end)).*0.5./(h/2)^2).^-1;                  % iterative step size
-dtP = (1./eta + K./(h/2)^2).^-1;                                           % iterative step size
-Vel = [U(:)+UBG(:);W(:)+WBG(:);u(:);w(:)];                                 % combine all velocity components
-if step > 1
-    dt  = min(2*dto,CFL*min([h/2/max(abs(Vel)), ...
-              0.05./max(abs(Div_fV(:)))]));                                % physical time step
-end
+    + (zeta(:,1:end-1)+zeta(:,2:end)).*0.5./(h/2)^2).^-1;                  % U iterative step size
+dtP = (1./eta + K./(h/2)^2).^-1;                                           % P iterative step size
 
-clear etayi eIIvp Vel d dd
+Vel = [U(:)+UBG(:);W(:)+WBG(:);u(:);w(:)];                                 % combine all velocity components
+dt  = rmp.*CFL*min([h/2/max(abs(Vel)), 0.05./max(abs(Div_fV(:)))]);        % physical time step
+
+% clean workspace
+clear Vel k kk etav etay zetav zetamin zetay
