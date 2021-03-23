f3986fdd862a986bda245955b8482240feb4ac9d
Tobias Keller <kellertobs@gmail.com>
2021-03-23 07:50:07 +0100

    fix issue with closing/reopening figures

--- src/output.m
+++ src/output.m
@@ -2,24 +2,6 @@
 frame = floor(step/nop);
 fprintf('\n*****  preparing output frame %d for %s \n',frame,runID);
 
-try close(fh1); catch; end
-try close(fh2); catch; end
-try close(fh3); catch; end
-clear fh1 fh2 fh3;
-
-if save_op
-    name = ['../out/',runID,'/',runID,'_cont'];
-    save([name,'.mat']);
-    name = ['../out/',runID,'/',runID,'_',num2str(frame)];
-    save([name,'.mat']);
-    
-    if step == 0
-        logfile = ['../out/',runID,'/',runID,'.log'];
-        if exist(logfile,'file'); delete(logfile); end
-        diary(logfile)
-    end
-end
-
 % prepare for plotting
 TX = {'Interpreter','Latex'}; FS = {'FontSize',18};
 TL = {'TickLabelInterpreter','Latex'}; TS = {'FontSize',14};
@@ -35,7 +17,7 @@ fw = axl + 3*axw + 2*ahs + axr;
 set(0,'DefaultFigureVisible',plot_op)
 
 % prepare and plot figure for solution fields
-fh1 = figure; colormap(ocean);
+fh1 = figure(1); clf; colormap(ocean);
 set(fh1,UN{:},'Position',[5 5 fw fh]);
 set(fh1,'PaperUnits','Centimeters','PaperPosition',[0 0 fw fh],'PaperSize',[fw fh]);
 set(fh1,'Color','w','InvertHardcopy','off');
@@ -69,7 +51,7 @@ set(cb,TL{:},TS{:}); set(gca,TL{:},TS{:}); title(['comp. pressure $p$'],TX{:},FS
 drawnow;
 
 % prepare and plot figure for material properties
-fh2 = figure; colormap(ocean);
+fh2 = figure(2); clf; colormap(ocean);
 set(fh2,UN{:},'Position',[10 10 fw fh]);
 set(fh2,'PaperUnits','Centimeters','PaperPosition',[0 0 fw fh],'PaperSize',[fw fh]);
 set(fh2,'Color','w','InvertHardcopy','off');
@@ -104,7 +86,7 @@ drawnow;
 
 % prepare and plot figure for solution residuals
 if plot_cv
-    fh3 = figure; colormap(ocean);
+    fh3 = figure(3); clf; colormap(ocean);
     fw = axl + 2*axw + 1*ahs + axr;
     set(fh3,UN{:},'Position',[15 15 fw fh]);
     set(fh3,'PaperUnits','Centimeters','PaperPosition',[0 0 fw fh],'PaperSize',[fw fh]);
@@ -131,21 +113,21 @@ if plot_cv
         drawnow;
     else
         set(fh3, 'CurrentAxes', ax(1))
-        imagesc(xc,zc,(-res_W.*dtW  ./(1e-16+norm(W(:)+WBG(:),2)./N))); axis ij equal tight; box on; cb = colorbar;
+        imagesc(xc,zc,(dW./(1e-16+norm(W(:)+WBG(:),2)./N))); axis ij equal tight; box on; cb = colorbar;
         set(cb,TL{:},TS{:}); set(gca,TL{:},TS{:}); title(['res. matrix z-velocity $W$'],TX{:},FS{:}); set(gca,'XTickLabel',[]);
         set(fh3, 'CurrentAxes', ax(2))
-        imagesc(xc,zc,(-res_U.*dtU  ./(1e-16+norm(U(:)+UBG(:),2)./N))); axis ij equal tight; box on; cb = colorbar;
+        imagesc(xc,zc,(dU./(1e-16+norm(U(:)+UBG(:),2)./N))); axis ij equal tight; box on; cb = colorbar;
         set(cb,TL{:},TS{:}); set(gca,TL{:},TS{:}); title(['res. matrix x-velocity $U$'],TX{:},FS{:}); set(gca,'XTickLabel',[]); set(gca,'YTickLabel',[]);
         set(fh3, 'CurrentAxes', ax(3))
-        imagesc(xc,zc,(-res_P.*dtP  ./(1e-16+norm(P(:),2)       ./N))); axis ij equal tight; box on; cb = colorbar;
+        imagesc(xc,zc,(dP./(1e-16+norm(P(:),2)       ./N))); axis ij equal tight; box on; cb = colorbar;
         set(cb,TL{:},TS{:}); set(gca,TL{:},TS{:}); title(['res. pore pressure $P$'],TX{:},FS{:});
         set(fh3, 'CurrentAxes', ax(4))
-        imagesc(xc,zc,(-res_f.*dt/10./(1e-16+norm(f(:),2)       ./N))); axis ij equal tight; box on; cb = colorbar;
+        imagesc(xc,zc,(df./(1e-16+norm(f(:),2)       ./N))); axis ij equal tight; box on; cb = colorbar;
         set(cb,TL{:},TS{:}); set(gca,TL{:},TS{:}); title(['res. liquid fraction $\phi$'],TX{:},FS{:}); set(gca,'YTickLabel',[]);
         drawnow;
     end
     
-    figure(200); clf;
+    figure(4); clf;
     p0 = linspace(-1,max(p(:)),1e3);
     plot(p0,eps0.*ones(size(p0)),'k',p0,1+p0+bnchmrk*5,'r',p(:),yieldt(:),'r.','LineWidth',2); axis equal tight; box on; hold on;
     scatter(p(:),tau(:),20,(eta(:)),'filled'); colorbar; colormap(ocean);
@@ -155,10 +137,22 @@ end
 
 % save output frame
 if save_op
-    clear ax cb fw axw axh avs ahs axl axr axt axb fh fw TX TL FS TS UN p0 dWi dWii dUi dUii dPi dPii
-
     name = ['../out/',runID,'/',runID,'_sol_',num2str(frame)];
     print(fh1,name,'-dpng','-r300');
     name = ['../out/',runID,'/',runID,'_mat_',num2str(frame)];
     print(fh2,name,'-dpng','-r300');
+    
+    clear fh1 fh2 fh3 ax cb fw axw axh avs ahs axl axr axt axb fh fw TX TL FS TS UN p0 dWi dWii dUi dUii dPi dPii
+    clear Wi Ui Pi dtWi dtUi dtPi dtWii dtUii dtPii fo Div_fVo Div_fVBGo Div_tz Div_tx dtW dtU dtP etac k kk
+
+    name = ['../out/',runID,'/',runID,'_cont'];
+    save([name,'.mat']);
+    name = ['../out/',runID,'/',runID,'_',num2str(frame)];
+    save([name,'.mat']);
+    
+    if step == 0
+        logfile = ['../out/',runID,'/',runID,'.log'];
+        if exist(logfile,'file'); delete(logfile); end
+        diary(logfile)
+    end
 end
