eee7e9e107d5795c162984c574a5ebb1c73d93b0
Tobias Keller <Tobias.Keller@glasgow.ac.uk>
2021-04-15 09:49:16 +0200

    updated demo parameter set

--- usr/run_demo.m
+++ usr/run_demo.m
@@ -1,9 +1,9 @@
 clear; close all;                % #ok<*NASGU> 
 
 % set run parameters
-runID    = 'demoN500';           % run identifier
+runID    = 'demo';               % run identifier
 restart  =  0;                   % restart from file (0: new run; <1: restart from last; >1: restart from specified frame)
-nop      =  10;                  % output frame plotted/saved every 'nop' time steps
+nop      =  1;                   % output frame plotted/saved every 'nop' time steps
 plot_op  =  1;                   % switch on (1) to display results
 save_op  =  1;                   % switch on (1) to save output to file
 plot_cv  =  1;                   % switch on (1) to live plot iterative convergence
@@ -12,19 +12,19 @@ demean   =  1;                   % remove mean from solution fields
 
 % set model domain parameters
 L        =  100;                 % domain dimension
-N        =  500 + 2;             % number of grid points in z-direction (incl. 2 ghosts)
+N        =  200 + 2;             % number of grid points in z-direction (incl. 2 ghosts)
 h        =  L/(N-2);             % grid spacing
 
 % set model timing parameters
-M        =  200;                 % number of time steps to take
+M        =  10;                  % number of time steps to take
 tend     =  1;                   % end time for simulation [s]
 
 % set model liquid fraction parameters
-f0       =  0.04;                % background liquid fraction
+f0       =  0.01;                % background liquid fraction
 f1       =  0.25;                % amplitude of random noise
 f2       =  0.00;                % amplitude of gaussian
-smx      =  (N/75)^2;            % smoothness of initial random noise in x-direction
-smz      =  (N/75)^2;            % smoothness of initial random noise in z-direction
+smx      =  (N/50)^2;            % smoothness of initial random noise in x-direction
+smz      =  (N/50)^2;            % smoothness of initial random noise in z-direction
 wx       =  L/5;                 % horizontal half-width of gaussian
 wz       =  L/5;                 % vertical half-width of initial gaussian
 xpos     =  0;                   % x-position of initial gaussian (0 = middle of domain)
@@ -36,9 +36,9 @@ n        =  3;                   % non-Newtonian shear viscosity powerlaw
 lmd      =  30;                  % shear viscosity liquid-weakening parameter
 
 % stress control parameters
-Pu       =  0;                   % ratio of pure-shear stress to buoyancy pressure
-Si       =  1/2;                 % ratio of simple-shear stress to buoyancy pressure
-B        =  10;                  % ratio of buoyancy force to 
+Pu       = -2;                   % ratio of pure-shear stress to buoyancy pressure
+Si       =  0;                   % ratio of simple-shear stress to buoyancy pressure
+B        =  1;                   % ratio of buoyancy force to 
 
 % set numerical model parameters
 nup      =  25;                  % nonlinear coefficients, residual norms updated every 'nup' iterations
@@ -48,11 +48,11 @@ theta    =  0.50;                % time-stepping scheme selector (1=BE, 1/2=CN, 
 rtol     =  1e-6;                % outer its relative tolerance
 atol     =  1e-6;                % outer its absolute tolerance
 minit    =  500;                 % minimum solver iterations
-maxit    =  2000;                % maximum solver iterations
+maxit    =  2500;                % maximum solver iterations
 alpha    =  0.99;                % inner its step size (fraction of stable step) [0,1]
 beta     =  0.85;                % iterative damping parameter (fraction of previous step) [0,1]
-gamma    =  0.99;                % iterative relaxation for rheology updates [0,1]
-kappa    =  1.00;                % regularisation of eIIvp for failure [0,1]
+gamma    =  0.90;                % iterative relaxation for rheology updates [0,1]
+kappa    =  0.00;                % regularisation of effective viscosities [0,1]
 etamin   =  1e-2;                % minimum viscosity for regularisation
 
 % create output directory
